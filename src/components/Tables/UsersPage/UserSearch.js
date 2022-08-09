0d42013a6baa34448925f97e627e259f38f9e4bc
kongnayeon <seoultownroad@naver.com>
2022-08-09 17:32:49 +0900

    :rocket: feat: 검색 완료 메시지, 총 유저 수, 관리자 색상 #30

--- src/components/Tables/UsersPage/UserSearch.js
+++ src/components/Tables/UsersPage/UserSearch.js
@@ -1,6 +1,7 @@
-import { Input, Select } from 'antd';
-import React, { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { UserOutlined } from '@ant-design/icons';
+import { Input, Select, Tag, Space, message } from 'antd';
+import React, { useEffect, useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { updateOption } from '../../../state/actions-creators/usersPage';
 const { Option } = Select;
 
@@ -8,6 +9,8 @@ function UserSearch() {
   const dispatch = useDispatch();
   const [searchOption, setSearchOption] = useState('searchName');
   const [searchString, setSearchString] = useState('');
+  const isMounted = useRef(false);
+  const { data } = useSelector(state => state.usersPage);
 
   const onOptionChange = e => {
     if (e === '1') {
@@ -34,30 +37,45 @@ function UserSearch() {
         searchString: searchString
       })
     );
+    if (!isMounted.current) {
+      isMounted.current = true;
+    } else {
+      message.success('검색 완료');
+    }
   }, [searchOption, searchString]);
 
   return (
     <>
       <div className="site-input-group-wrapper">
-        <Input.Group compact>
-          <Select defaultValue="입금자명" onChange={onOptionChange}>
-            <Option key="1" value="1">
-              입금자명
-            </Option>
-            <Option key="2" value="2">
-              전화번호
-            </Option>
-          </Select>
-          <Input.Search
+        <Space size="middle">
+          <Input.Group
+            compact
             style={{
-              width: 'calc(100% - 200px)'
+              display: 'flex'
             }}
-            placeholder="search text"
-            onSearch={onSearch}
-            onKeyPress={handleEnter}
-          />
-        </Input.Group>
-        <br />
+          >
+            <Select defaultValue="입금자명" onChange={onOptionChange}>
+              <Option key="1" value="1">
+                입금자명
+              </Option>
+              <Option key="2" value="2">
+                전화번호
+              </Option>
+            </Select>
+
+            <Input.Search
+              style={{
+                width: 'calc(100%)'
+              }}
+              placeholder="search text"
+              onSearch={onSearch}
+              onKeyPress={handleEnter}
+            />
+          </Input.Group>
+          <Tag icon={<UserOutlined />} color="default">
+            총 유저 수: {data ? data.total : ''}
+          </Tag>
+        </Space>
       </div>
     </>
   );
