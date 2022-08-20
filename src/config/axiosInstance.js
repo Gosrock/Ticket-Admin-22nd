3e5357d1392472c93776134c1ae8071a05cb1bbf
이찬진 <water0641@naver.com>
2022-08-20 16:20:14 +0900

    :hammer: fix(config) : 로컬 스토릿지 어세스토큰 키이름 수정

--- src/config/axiosInstance.js
+++ src/config/axiosInstance.js
@@ -1,26 +1,26 @@
-import axios from "axios";
+import axios from 'axios';
 
 axios.interceptors.response.use(
-  (response) => {
+  response => {
     return response;
   },
-  async (error) => {
+  async error => {
     const {
-      response: { status },
+      response: { status }
     } = error;
     if (status === 401) {
       //401 이면 토큰오류이므로 해당으로 통일.
 
       // 로컬 스토리지에 어세스토큰 삭제
-      localStorage.removeItem("adminAccessToken");
+      localStorage.removeItem('accessToken');
       // token refresh 요청
 
       // 새로운 토큰 저장
       axios.defaults.headers.common.Authorization = null;
       // 401로 요청 실패했던 요청 새로운 accessToken으로 재요청
+      alert('자동 로그아웃 되셨습니다. 다시 로그인 해주세요.');
 
-      alert("자동 로그아웃 되셨습니다. 다시 로그인 해주세요.");
-      window.location.href = "/auth/login";
+      window.location.href = '/auth/login';
     }
     return Promise.reject(error);
   }
